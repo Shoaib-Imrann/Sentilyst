a4f73d018a3cb332634919649e80e650bf84e2ae
Shoaib-Imrann <irexceed@gmail.com>
2025-06-26 16:49:29 +0530

    feat: sentry integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './Context/AppContext.jsx'
+import * as Sentry from "@sentry/react";
+
+const dsn = import.meta.env.VITE_SENTRY_DSN;
+
+if(dsn){
+Sentry.init({
+  dsn: dsn,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      colorScheme: "dark",
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+}
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
